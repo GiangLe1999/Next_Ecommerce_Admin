8657e4d2dffdd8774f483a8797f023a103f5c596
GiangLe1999 <legiangbmt09@gmail.com>
2023-05-07 19:47:43 +0700

    fix input outline color

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,15 @@
 import mongoose, { model, models, Schema } from "mongoose";
 
-const ProductSchema = new Schema({
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: { type: String },
-  images: [{ type: String }],
-  category: { type: mongoose.Types.ObjectId, ref: "Category" },
-  properties: { type: Object },
-});
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String },
+    images: [{ type: String }],
+    category: { type: mongoose.Types.ObjectId, ref: "Category" },
+    properties: { type: Object },
+  },
+  { timestamps: true }
+);
 
 export const Product = models.Product || model("Product", ProductSchema);
